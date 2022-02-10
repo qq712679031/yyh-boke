8cd680526ca8231238b19257f6857ef1377d6495
yangyunhuang <yangyunhuang@zhugeio.com>
2022-02-10 18:32:14 +0800

    kafka

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,56 @@
 module.exports = {
     // 站点配置
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
+    title: '你好！',
     description: '这是我的第一个 VuePress 站点',
 
     // 主题和它的配置
     theme: '@vuepress/theme-default',
     themeConfig: {
-        logo: 'https://vuejs.org/images/logo.png',
+        // logo: 'https://vuejs.org/images/logo.png',
+        //导航栏
+        navbar: [
+            {
+                text: '消息队列',
+                children: [
+                    {
+                        text: 'kafka',
+                        children: [
+                            '/MQ/kafka/kafkaCommand.md',
+                            '/MQ/kafka/sasl/kafkaSaslPlain.md',
+                        ],
+                    },
+                    // {
+                    //     text: 'RabbitMQ',
+                    //     children: [
+                    //         '/zh/reference/bundler/vite.md',
+                    //         '/zh/reference/bundler/webpack.md',
+                    //     ],
+                    // },
+                ],
+            }
+        ],
+        //侧边栏
+        // sidebar: {
+        //     '/zh/guide/': [
+        //         {
+        //             text: '指南',
+        //             children: [
+        //                 '/zh/guide/README.md',
+        //                 '/zh/guide/getting-started.md',
+        //                 '/zh/guide/configuration.md',
+        //                 '/zh/guide/page.md',
+        //                 '/zh/guide/markdown.md',
+        //                 '/zh/guide/assets.md',
+        //                 '/zh/guide/i18n.md',
+        //                 '/zh/guide/deployment.md',
+        //                 '/zh/guide/theme.md',
+        //                 '/zh/guide/plugin.md',
+        //                 '/zh/guide/bundler.md',
+        //                 '/zh/guide/migration.md',
+        //             ],
+        //         },
+        //     ],
+        // }
     },
 }
